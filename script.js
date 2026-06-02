4d374ef85ab52d84977b8a5da79a830fd2703436
louanbelo-svg <louanbelo@gmail.com>
2026-06-02 09:37:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
+const progressBar = document.querySelector(".progress-bar");
 
+window.addEventListener("scroll", () => {
+
+const scroll =
+document.documentElement.scrollTop;
+
+const height =
+document.documentElement.scrollHeight -
+document.documentElement.clientHeight;
+
+const progress =
+(scroll / height) * 100;
+
+progressBar.style.width = progress + "%";
+
+});
+
+const backToTop =
+document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+if(window.scrollY > 500){
+
+backToTop.style.display = "block";
+
+}else{
+
+backToTop.style.display = "none";
+
+}
+
+});
+
+backToTop.addEventListener("click", () => {
+
+window.scrollTo({
+top:0,
+behavior:"smooth"
+});
+
+});
+
+const reveals =
+document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+
+reveals.forEach(section => {
+
+const top =
+section.getBoundingClientRect().top;
+
+if(top < window.innerHeight - 100){
+
+section.classList.add("reveal");
+section.classList.add("active");
+
+}
+
+});
+
+});
